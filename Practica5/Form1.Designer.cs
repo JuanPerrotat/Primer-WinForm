91875fdb62d394c2608dcad9ea9861c2e754c810
JuanPerrotat <juanperrotat@gmail.com>
2025-11-17 14:29:54 -0300

    Upper eliminado

--- Practica5/Form1.Designer.cs
+++ Practica5/Form1.Designer.cs
@@ -48,10 +48,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lblApellido.AutoSize = true;
             this.lblApellido.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblApellido.Location = new System.Drawing.Point(32, 41);
-            this.lblApellido.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblApellido.Location = new System.Drawing.Point(43, 50);
             this.lblApellido.Name = "lblApellido";
-            this.lblApellido.Size = new System.Drawing.Size(69, 19);
+            this.lblApellido.Size = new System.Drawing.Size(80, 23);
             this.lblApellido.TabIndex = 0;
             this.lblApellido.Text = "Apellido:";
             // 
@@ -61,10 +60,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lblNombre.AutoSize = true;
             this.lblNombre.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNombre.Location = new System.Drawing.Point(32, 82);
-            this.lblNombre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNombre.Location = new System.Drawing.Point(43, 101);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(67, 19);
+            this.lblNombre.Size = new System.Drawing.Size(80, 23);
             this.lblNombre.TabIndex = 1;
             this.lblNombre.Text = "Nombre:";
             // 
@@ -74,10 +72,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lblEdad.AutoSize = true;
             this.lblEdad.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEdad.Location = new System.Drawing.Point(32, 119);
-            this.lblEdad.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEdad.Location = new System.Drawing.Point(43, 146);
             this.lblEdad.Name = "lblEdad";
-            this.lblEdad.Size = new System.Drawing.Size(46, 19);
+            this.lblEdad.Size = new System.Drawing.Size(54, 23);
             this.lblEdad.TabIndex = 2;
             this.lblEdad.Text = "Edad:";
             // 
@@ -87,10 +84,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lblDireccion.AutoSize = true;
             this.lblDireccion.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDireccion.Location = new System.Drawing.Point(32, 151);
-            this.lblDireccion.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDireccion.Location = new System.Drawing.Point(43, 186);
             this.lblDireccion.Name = "lblDireccion";
-            this.lblDireccion.Size = new System.Drawing.Size(75, 19);
+            this.lblDireccion.Size = new System.Drawing.Size(87, 23);
             this.lblDireccion.TabIndex = 3;
             this.lblDireccion.Text = "Dirección:";
             // 
@@ -98,12 +94,11 @@
             // 
             this.txtbApellido.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtbApellido.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.txtbApellido.Location = new System.Drawing.Point(111, 40);
-            this.txtbApellido.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtbApellido.Location = new System.Drawing.Point(148, 49);
+            this.txtbApellido.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtbApellido.MaxLength = 30;
             this.txtbApellido.Name = "txtbApellido";
-            this.txtbApellido.Size = new System.Drawing.Size(338, 20);
+            this.txtbApellido.Size = new System.Drawing.Size(449, 22);
             this.txtbApellido.TabIndex = 4;
             this.txtbApellido.TextChanged += new System.EventHandler(this.txtbApellido_TextChanged);
             // 
@@ -111,12 +106,11 @@
             // 
             this.txtboxNombre.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtboxNombre.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.txtboxNombre.Location = new System.Drawing.Point(111, 82);
-            this.txtboxNombre.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtboxNombre.Location = new System.Drawing.Point(148, 101);
+            this.txtboxNombre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtboxNombre.MaxLength = 30;
             this.txtboxNombre.Name = "txtboxNombre";
-            this.txtboxNombre.Size = new System.Drawing.Size(338, 20);
+            this.txtboxNombre.Size = new System.Drawing.Size(449, 22);
             this.txtboxNombre.TabIndex = 5;
             this.txtboxNombre.TextChanged += new System.EventHandler(this.txtboxNombre_TextChanged);
             // 
@@ -124,11 +118,11 @@
             // 
             this.txtboxEdad.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtboxEdad.Location = new System.Drawing.Point(111, 118);
-            this.txtboxEdad.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtboxEdad.Location = new System.Drawing.Point(148, 145);
+            this.txtboxEdad.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtboxEdad.MaxLength = 30;
             this.txtboxEdad.Name = "txtboxEdad";
-            this.txtboxEdad.Size = new System.Drawing.Size(338, 20);
+            this.txtboxEdad.Size = new System.Drawing.Size(449, 22);
             this.txtboxEdad.TabIndex = 6;
             this.txtboxEdad.TextChanged += new System.EventHandler(this.txtboxEdad_TextChanged);
             this.txtboxEdad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtboxEdad_KeyPress);
@@ -137,12 +131,11 @@
             // 
             this.txtboxDireccion.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtboxDireccion.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.txtboxDireccion.Location = new System.Drawing.Point(111, 151);
-            this.txtboxDireccion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtboxDireccion.Location = new System.Drawing.Point(148, 186);
+            this.txtboxDireccion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtboxDireccion.MaxLength = 30;
             this.txtboxDireccion.Name = "txtboxDireccion";
-            this.txtboxDireccion.Size = new System.Drawing.Size(338, 20);
+            this.txtboxDireccion.Size = new System.Drawing.Size(449, 22);
             this.txtboxDireccion.TabIndex = 7;
             this.txtboxDireccion.TextChanged += new System.EventHandler(this.txtboxDireccion_TextChanged);
             // 
@@ -152,10 +145,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lblResultado.AutoSize = true;
             this.lblResultado.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblResultado.Location = new System.Drawing.Point(32, 206);
-            this.lblResultado.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblResultado.Location = new System.Drawing.Point(43, 254);
             this.lblResultado.Name = "lblResultado";
-            this.lblResultado.Size = new System.Drawing.Size(78, 19);
+            this.lblResultado.Size = new System.Drawing.Size(93, 23);
             this.lblResultado.TabIndex = 8;
             this.lblResultado.Text = "Resultado:";
             // 
@@ -163,10 +155,10 @@
             // 
             this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.richTextBox1.Location = new System.Drawing.Point(35, 251);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBox1.Location = new System.Drawing.Point(47, 309);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(404, 110);
+            this.richTextBox1.Size = new System.Drawing.Size(537, 134);
             this.richTextBox1.TabIndex = 10;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
@@ -176,10 +168,10 @@
             this.btnAceptar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAceptar.Location = new System.Drawing.Point(134, 415);
-            this.btnAceptar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAceptar.Location = new System.Drawing.Point(179, 511);
+            this.btnAceptar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnAceptar.Name = "btnAceptar";
-            this.btnAceptar.Size = new System.Drawing.Size(78, 29);
+            this.btnAceptar.Size = new System.Drawing.Size(104, 36);
             this.btnAceptar.TabIndex = 11;
             this.btnAceptar.Text = "Aceptar";
             this.btnAceptar.UseVisualStyleBackColor = true;
@@ -190,10 +182,10 @@
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancel.Location = new System.Drawing.Point(259, 415);
-            this.btnCancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCancel.Location = new System.Drawing.Point(345, 511);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(78, 29);
+            this.btnCancel.Size = new System.Drawing.Size(104, 36);
             this.btnCancel.TabIndex = 12;
             this.btnCancel.Text = "Cancelar";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -201,9 +193,9 @@
             // 
             // Practica5
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(536, 496);
+            this.ClientSize = new System.Drawing.Size(715, 610);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnAceptar);
             this.Controls.Add(this.richTextBox1);
@@ -216,8 +208,8 @@
             this.Controls.Add(this.lblEdad);
             this.Controls.Add(this.lblNombre);
             this.Controls.Add(this.lblApellido);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.MinimumSize = new System.Drawing.Size(552, 535);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MinimumSize = new System.Drawing.Size(730, 648);
             this.Name = "Practica5";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Datos Personales";
